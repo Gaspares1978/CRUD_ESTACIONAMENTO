a57ce4515b9e14f3856593bca8849ae2b56bd992
gaspar <gaspares1978@gmail.com>
2019-09-13 09:02:53 -0300

    crud de um estacionamento

---
+++ Estacionamento/Estacionamento/Controllers/carrosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Estacionamento.Models;
+
+namespace Estacionamento.Controllers
+{
+    public class carrosController : Controller
+    {
+        private contexto db = new contexto();
+
+        // GET: carros
+        public ActionResult Index()
+        {
+            return View(db.Carros.ToList());
+        }
+
+        // GET: carros/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            carros carros = db.Carros.Find(id);
+            if (carros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carros);
+        }
+
+        // GET: carros/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: carros/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Marca,Modelo,Ano,Placa,Cor,Proprietario,Endereço,Entrada,Saida,Permanencia")] carros carros)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Carros.Add(carros);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(carros);
+        }
+
+        // GET: carros/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            carros carros = db.Carros.Find(id);
+            if (carros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carros);
+        }
+
+        // POST: carros/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Marca,Modelo,Ano,Placa,Cor,Proprietario,Endereço,Entrada,Saida,Permanencia")] carros carros)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(carros).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(carros);
+        }
+
+        // GET: carros/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            carros carros = db.Carros.Find(id);
+            if (carros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carros);
+        }
+
+        // POST: carros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            carros carros = db.Carros.Find(id);
+            db.Carros.Remove(carros);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Estacionamento/Estacionamento/Models/carros.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Estacionamento.Models
+{
+    public class carros
+    {
+        [Key]
+        public int id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public string Placa { get; set; }
+        public string Cor { get; set; }
+        public string Proprietario { get; set; }
+        public string Endereço { get; set; }
+        public string Entrada { get; set; }
+        public string Saida { get; set; }
+        public string Permanencia { get; set; }
+    }
+}
